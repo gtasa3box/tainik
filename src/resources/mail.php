dd2bfc9b376f5e5b2ac381d800aafdacc5e51b46
gtasa3box <gtasa3box@gmail.com>
2022-03-01 15:36:57 +0300

    com

--- src/resources/mail.php
+++ src/resources/mail.php
@@ -1,69 +1,56 @@
 <?php
-// Файлы phpmailer
-require 'phpmailer/PHPMailer.php';
-require 'phpmailer/SMTP.php';
-require 'phpmailer/Exception.php';
 
-$title = "Тема письма";
-$file = $_FILES['file'];
+include("SxGeo.php");
 
+$method = $_SERVER['REQUEST_METHOD'];
+
+//Script Foreach
 $c = true;
-// Формирование самого письма
-$title = "Заголовок письма";
-foreach ( $_POST as $key => $value ) {
-  if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-    $body .= "
-    " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-      <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-      <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-    </tr>
-    ";
-  }
+if ( $method === 'POST' ) {
+	$admin_email = 'info@safe-etalon.ru';
+	$form_subject = 'taynik.safe-etalon.ru';
+
+	$massive = [];
+
+	$ip=$_SERVER['REMOTE_ADDR'];
+
+	$SxGeo = new SxGeo('SxGeoCity.dat');
+
+	
+
+	$GeoPos = $SxGeo->getCity($ip);
+
+	$massive['Geo'] = $GeoPos['city']['name_ru'];
+
+	$massive['Geo'] .= ' ('.$ip.')';
+
+	$massive['number']  = htmlspecialchars(trim($_POST["number"]), ENT_QUOTES);
+
+
+	foreach ( $massive as $key => $value ) {
+		if ( $value != "") {
+			$message .= "
+			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+			</tr>
+			";
+		}
+	}
 }
 
-$body = "<table style='width: 100%;'>$body</table>";
+$message = "<table style='width: 100%;'>$message</table>";
 
-// Настройки PHPMailer
-$mail = new PHPMailer\PHPMailer\PHPMailer();
-
-try {
-  $mail->isSMTP();
-  $mail->CharSet = "UTF-8";
-  $mail->SMTPAuth   = true;
-
-  // Настройки вашей почты
-  $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
-  $mail->Username   = ''; // Логин на почте
-  $mail->Password   = ''; // Пароль на почте
-  $mail->SMTPSecure = 'ssl';
-  $mail->Port       = 465;
-
-  $mail->setFrom('', 'Заявка с вашего сайта'); // Адрес самой почты и имя отправителя
-
-  // Получатель письма
-  $mail->addAddress('');
-
-  // Прикрипление файлов к письму
-  if (!empty($file['name'][0])) {
-    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
-      $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
-      $filename = $file['name'][$ct];
-      if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
-          $mail->addAttachment($uploadfile, $filename);
-          $rfile[] = "Файл $filename прикреплён";
-      } else {
-          $rfile[] = "Не удалось прикрепить файл $filename";
-      }
-    }
-  }
-
-  // Отправка сообщения
-  $mail->isHTML(true);
-  $mail->Subject = $title;
-  $mail->Body = $body;
-
-  $mail->send();
-
-} catch (Exception $e) {
-  $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+function adopt($text) {
+	return '=?UTF-8?B?'.Base64_encode($text).'?=';
 }
+
+$headers = "MIME-Version: 1.0" . PHP_EOL .
+"Content-Type: text/html; charset=utf-8" . PHP_EOL .
+'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
+'Reply-To: '.$admin_email.'' . PHP_EOL;
+
+mail($admin_email, adopt($form_subject), $message, $headers );
+
+header('Location: http://'.$_SERVER['HTTP_HOST'].'/');
+exit;
